3b9c27512936b8f6c796e2b458144c517224bc7b
Saaamuuuuel <sgm1027@alu.ubu.es>
2023-12-27 13:11:50 +0100

    traducción2

--- PDF-Scrapping.R
+++ PDF-Scrapping.R
@@ -1,297 +1,295 @@
-const fitz = require('fitz');
-const re = require('re');
-const pd = require('pandas');
-const os = require('os');
-const np = require('numpy');
-const CarpetaEntrada = "INPUT";
-const CarpetaDatos = "DATOS";
-const CarpetaInformes = "INFORMES";
-const CarpetaSalida = "OUTPUT";
-const CarpetaResultados = "RESULTADOS";
-const PathBase = os.getcwd();
+install.packages("pdftools");
+install.packages("tidyverse");
+install.packages("stringr");
+install.packages("readxl");
 
-function LeerFicherosPDF(ruta) {
-  const ficheros = [];
-  let subcarpetas = [];
-  for (const [raiz, directorios, archivos] of os.walk(ruta)) {
-    subcarpetas = directorios.sort((a, b) => parseInt(a.replace(/\D/g, '')) - parseInt(b.replace(/\D/g, '')));
+library(pdftools)
+library(stringr)
+library(readxl)
+
+CarpetaEntrada <- "INPUT"
+CarpetaDatos <- "DATOS"
+CarpetaInformes <- "INFORMES"
+CarpetaSalida <- "OUTPUT"
+CarpetaResultados <- "RESULTADOS"
+PathBase <- getwd()
+
+LeerFicherosPDF <- function(ruta) {
+  ficheros <- list.files(path = ruta, pattern = "\\.pdf$", full.names = TRUE, recursive = TRUE)
+  return(ficheros)
+}
+
+LeerDocumento <- function(nombreFichero) {
+  doc <- pdf_text(nombreFichero)
+  text <- paste(doc, collapse = "\n")
+  return(strsplit(text, "\n")[[1]])
+}
+
+BuscarValor <- function(textoBuscar, lines) {
+  Encontrados <- character()
+  valores <- ifelse(grepl(textoBuscar, lines), 1, 0)
+  valores <- which(valores == 1)
+  posiciones <- nchar(textoBuscar)
+  for (i in valores) {
+    Encontrados <- c(Encontrados, trimws(substring(lines[i], nchar(textoBuscar) + 1)))
   }
-  for (const subcarpeta of subcarpetas) {
-    const subcarpeta_ruta = os.path.normpath(os.path.join(raiz, subcarpeta));
-  }
-  for (const archivo of os.listdir(subcarpeta_ruta)) {
-    if (archivo.endsWith('.pdf')) {
-      ficheros.push(os.path.normpath(os.path.join(subcarpeta_ruta, archivo)));
+  return(Encontrados)
+}
+
+fichero <- file.path(PathBase, CarpetaEntrada, CarpetaDatos, "Diagnostico.xlsx")
+diagnostico <- read_excel(fichero)
+diagnosticos_dic <- setNames(diagnostico$`NÚMERO DIAGNÓSTICO`, diagnostico$DIAGNÓSTICO)
+for (diagnostico in names(diagnosticos_dic)) {
+  valor <- diagnosticos_dic[[diagnostico]]
+  print(paste(diagnostico, valor))
+}
+
+fichero <- file.path(PathBase, CarpetaEntrada, CarpetaDatos, "Genes.xlsx")
+genes <- read_excel(fichero)
+mutaciones <- unique(genes$GEN)
+mutaciones_dic <- setNames(genes$`Número gen`, genes$GEN)
+for (gen in names(mutaciones_dic)) {
+  valor <- mutaciones_dic[[gen]]
+  print(paste(gen, valor))
+}
+
+rutaEntrada <- file.path(PathBase, CarpetaEntrada, CarpetaInformes)
+ficheros <- LeerFicherosPDF(rutaEntrada)
+NHC_Data <- list()
+Nbiopsia_Data <- list()
+fecha_Data <- list()
+texto_Data <- list()
+for (ficheroPDF in ficheros) {
+  lines <- LeerDocumento(ficheroPDF)
+  NHC_Data[[length(NHC_Data) + 1]] <- BuscarValor("NHC:", lines)
+  Nbiopsia_Data[[length(Nbiopsia_Data) + 1]] <- BuscarValor("biopsia:", lines)
+  fecha_Data[[length(fecha_Data) + 1]] <- BuscarValor("Fecha:", lines)
+  texto_Data[[length(texto_Data) + 1]] <- BuscarValor("de la muestra:", lines)
+}
+print(NHC_Data)
+print(Nbiopsia_Data)
+print(fecha_Data)
+print(texto_Data)
+
+textoDiag <- character()
+numeroDiag <- numeric()
+for (i in texto_Data) {
+  sinduplicados <- unique(i)
+  textoDiag <- c(textoDiag, sinduplicados[1])
+}
+print(textoDiag)
+for (diagnostico in textoDiag) {
+  valor <- diagnosticos_dic[[diagnostico]]
+  numeroDiag <- c(numeroDiag, valor)
+}
+print(numeroDiag)
+
+NHC <- character()
+for (i in NHC_Data) {
+  sinduplicadosNHC <- unique(i)
+  NHC <- c(NHC, sinduplicadosNHC[1])
+}
+print(NHC)
+
+lista_resultante <- list()
+elementos_vistos <- character()
+for (sublist in Nbiopsia_Data) {
+  sublist_sin_duplicados <- character()
+  for (elemento in sublist) {
+    if (!(elemento %in% elementos_vistos)) {
+      sublist_sin_duplicados <- c(sublist_sin_duplicados, elemento)
     }
+    elementos_vistos <- c(elementos_vistos, elemento)
   }
-  for (const archivo of os.listdir(ruta)) {
-    if (archivo.endsWith('.pdf')) {
-      ficheros.push(os.path.normpath(os.path.join(ruta, archivo)));
-    }
-  }
-  return ficheros;
+  lista_resultante <- c(lista_resultante, sublist_sin_duplicados)
 }
+print(lista_resultante)
 
-function LeerDocumento(nombreFichero) {
-  const doc = fitz.open(nombreFichero);
-  let text = "";
-  for (const page of doc) {
-    text = text + page.get_text();
-  }
-  return text.split('\n');
-}
+NB_values <- unlist(lista_resultante)
+print(NB_values)
 
-function BuscarValor(textoBuscar, lines) {
-  const Encontrados = [];
-  const valores = lines.map(line => re.search(textoBuscar, line) ? 1 : 0);
-  const indices = valores.map((val, i) => val === 1 ? i : -1).filter(i => i !== -1);
-  const posiciones = textoBuscar.length;
-  for (const i of indices) {
-    Encontrados.push(lines[i].slice(lines[i].lastIndexOf(textoBuscar) + posiciones).trim());
-  }
-  return Encontrados;
-}
-
-const fichero = os.path.normpath(os.path.join(PathBase, CarpetaEntrada, CarpetaDatos, "Diagnostico.xlsx"));
-const diagnostico = pd.read_excel("INPUT/DATOS/Diagnostico.xlsx");
-const diagnosticos_dic = Object.fromEntries(diagnostico["DIAGNÓSTICO"].map((diag, i) => [diag, diagnostico["NÚMERO DIAGNÓSTICO"][i]]));
-for (const diagnostico in diagnosticos_dic) {
-  const valor = diagnosticos_dic[diagnostico];
-  console.log(diagnostico, valor);
-}
-
-const fichero = os.path.normpath(os.path.join(PathBase, CarpetaEntrada, CarpetaDatos, "Genes.xlsx"));
-const genes = pd.read_excel(fichero);
-const mutaciones = genes["GEN"].unique();
-const mutaciones_dic = Object.fromEntries(genes["GEN"].map((gen, i) => [gen, genes["Número gen"][i]]));
-for (const [gen, valor] of Object.entries(mutaciones_dic)) {
-  console.log(gen, valor);
-}
-
-const rutaEntrada = os.path.normpath(os.path.join(PathBase, CarpetaEntrada, CarpetaInformes));
-const fecha_Data = [];
-const NHC_Data = [];
-const Nbiopsia_Data = [];
-const texto_Data = [];
-const ficheros = LeerFicherosPDF(rutaEntrada);
-for (const ficheroPDF of ficheros) {
-  const lines = LeerDocumento(os.path.normpath(os.path.join(rutaEntrada, ficheroPDF)));
-  NHC_Data.push(BuscarValor("NHC:", lines));
-  Nbiopsia_Data.push(BuscarValor("biopsia:", lines));
-  fecha_Data.push(BuscarValor("Fecha:", lines));
-  texto_Data.push(BuscarValor("de la muestra:", lines));
-}
-console.log(NHC_Data);
-console.log(Nbiopsia_Data);
-console.log(fecha_Data);
-console.log(texto_Data);
-
-const textoDiag = [];
-const numeroDiag = [];
-for (const i of texto_Data) {
-  const sinduplicados = [...new Set(i)];
-  textoDiag.push(i.filter(x => sinduplicados.includes(x))[0]);
-}
-console.log(textoDiag);
-for (const diagnostico of textoDiag) {
-  const valor = diagnosticos_dic[diagnostico];
-  numeroDiag.push(valor);
-}
-console.log(numeroDiag);
-
-const NHC = [];
-for (const i of NHC_Data) {
-  const sinduplicadosNHC = [...new Set(i)];
-  NHC.push(i.filter(x => sinduplicadosNHC.includes(x))[0]);
-}
-console.log(NHC);
-
-const lista_resultante = [];
-const elementos_vistos = new Set();
-for (const sublist of Nbiopsia_Data) {
-  const sublist_sin_duplicados = [];
-  for (const elemento of sublist) {
-    if (!elementos_vistos.has(elemento)) {
-      sublist_sin_duplicados.push(elemento);
-    }
-    elementos_vistos.add(elemento);
-  }
-  lista_resultante.push(sublist_sin_duplicados);
-}
-console.log(lista_resultante);
-
-const NB_values = lista_resultante.flat();
-console.log(NB_values);
-
-const biopsia = NB_values.map(x => x[2]);
-const B = "1";
-const C = "3";
-const P = "2";
-const Biopsia_solida = [];
-for (const i of biopsia) {
-  if (i === "B") {
-    Biopsia_solida.push(B);
-    console.log("1");
-  } else if (i === "P") {
-    Biopsia_solida.push(P);
-    console.log("2");
+biopsia <- NB_values[seq(3, length(NB_values), 3)]
+B <- "1"
+C <- "3"
+P <- "2"
+Biopsia_solida <- character()
+for (i in biopsia) {
+  if (i == "B") {
+    Biopsia_solida <- c(Biopsia_solida, B)
+    print("1")
+  } else if (i == "P") {
+    Biopsia_solida <- c(Biopsia_solida, P)
+    print("2")
   } else {
-    Biopsia_solida.push(C);
-    console.log("3");
+    Biopsia_solida <- c(Biopsia_solida, C)
+    print("3")
   }
 }
-console.log(Biopsia_solida);
+print(Biopsia_solida)
 
-const fechas = [];
-for (const i of fecha_Data) {
-  const sinduplicados = [...new Set(i)];
-  fechas.push(i.filter(x => sinduplicados.includes(x))[0]);
+fechas <- character()
+for (i in fecha_Data) {
+  sinduplicados <- unique(i)
+  fechas <- c(fechas, sinduplicados[1])
 }
-console.log(fechas);
+print(fechas)
 
-const ficheros = LeerFicherosPDF(rutaEntrada);
-const lista_ensayos = [];
-const ensayos_finales = [];
-for (const ficheroPDF of ficheros) {
-  const lines = LeerDocumento(os.path.normpath(os.path.join(rutaEntrada, ficheroPDF)));
-  let ensayos = 0;
-  for (const line of lines) {
-    const resultado = line.match(patron);
-    if (resultado) {
-      ensayos = parseInt(resultado[1]);
+ficheros <- LeerFicherosPDF(rutaEntrada)
+lista_ensayos <- numeric()
+ensayos_finales <- numeric()
+for (ficheroPDF in ficheros) {
+  lines <- LeerDocumento(ficheroPDF)
+  ensayos <- 0
+  for (line in lines) {
+    resultado <- str_match(line, patron)
+    if (!is.na(resultado)) {
+      ensayos <- as.integer(resultado[2])
     }
   }
-  lista_ensayos.push(ensayos);
+  lista_ensayos <- c(lista_ensayos, ensayos)
 }
-console.log(lista_ensayos);
-for (const i of lista_ensayos) {
-  if (i === 0) {
-    ensayos_finales.push(0);
+print(lista_ensayos)
+for (i in lista_ensayos) {
+  if (i == 0) {
+    ensayos_finales <- c(ensayos_finales, 0)
   } else {
-    ensayos_finales.push(1);
+    ensayos_finales <- c(ensayos_finales, 1)
   }
 }
-console.log(ensayos_finales);
+print(ensayos_finales)
 
-const ficheros = LeerFicherosPDF(rutaEntrada);
-const lista_tratamientos = [];
-const tratamientos_finales = [];
-for (const ficheroPDF of ficheros) {
-  const lines = LeerDocumento(os.path.normpath(os.path.join(rutaEntrada, ficheroPDF)));
-  let tratamientos = 0;
-  for (const line of lines) {
-    const resultado = line.match(patron2);
-    if (resultado) {
-      tratamientos = parseInt(resultado[1]);
+ficheros <- LeerFicherosPDF(rutaEntrada)
+lista_tratamientos <- numeric()
+tratamientos_finales <- numeric()
+for (ficheroPDF in ficheros) {
+  lines <- LeerDocumento(ficheroPDF)
+  tratamientos <- 0
+  for (line in lines) {
+    resultado <- str_match(line, patron2)
+    if (!is.na(resultado)) {
+      tratamientos <- as.integer(resultado[2])
     }
   }
-  lista_tratamientos.push(tratamientos);
+  lista_tratamientos <- c(lista_tratamientos, tratamientos)
 }
-console.log(lista_tratamientos);
-for (const i of lista_tratamientos) {
-  if (i === 0) {
-    tratamientos_finales.push(0);
+print(lista_tratamientos)
+for (i in lista_tratamientos) {
+  if (i == 0) {
+    tratamientos_finales <- c(tratamientos_finales, 0)
   } else {
-    tratamientos_finales.push(1);
+    tratamientos_finales <- c(tratamientos_finales, 1)
   }
 }
-console.log(tratamientos_finales);
+print(tratamientos_finales)
 
-for (const ficheroPDF of ficheros) {
-  if (os.path.isfile(os.path.normpath(os.path.join(rutaEntrada, ficheroPDF))) && ficheroPDF.endsWith(".pdf")) {
-    console.log(ficheroPDF);
+ficheros <- LeerFicherosPDF(rutaEntrada)
+for (ficheroPDF in ficheros) {
+  if (file.exists(ficheroPDF) && file_ext(ficheroPDF) == ".pdf") {
+    print(ficheroPDF)
   }
 }
 
-const numero_paciente = [];
-for (const ficheroPDF of ficheros) {
-  if (os.path.isfile(os.path.normpath(os.path.join(rutaEntrada, ficheroPDF))) && ficheroPDF.endsWith(".pdf")) {
-    const [ruta1, fichero1] = os.path.split(ficheroPDF);
-    const paciente = os.path.splitext(fichero1)[0];
-    const pacientes = paciente[7];
-    numero_paciente.push(pacientes);
+numero_paciente <- character()
+for (ficheroPDF in ficheros) {
+  if (file.exists(ficheroPDF) && file_ext(ficheroPDF) == ".pdf") {
+    fichero1 <- basename(ficheroPDF)
+    paciente <- str_remove(fichero1, "\\.pdf$")
+    pacientes <- substr(paciente, 8, 8)
+    numero_paciente <- c(numero_paciente, pacientes)
   }
 }
-console.log(numero_paciente);
+print(numero_paciente)
 
-const chip2 = [];
-for (const ficheroPDF of ficheros) {
-  if (os.path.isfile(os.path.normpath(os.path.join(rutaEntrada, ficheroPDF))) && ficheroPDF.endsWith(".pdf")) {
-    const resultado = ficheroPDF.match(patron);
-    if (resultado) {
-      const numero_chip = resultado[1];
-      chip2.push(numero_chip);
+chip2 <- character()
+for (ficheroPDF in ficheros) {
+  if (file.exists(ficheroPDF) && file_ext(ficheroPDF) == ".pdf") {
+    resultado <- str_match(ficheroPDF, patron)
+    if (!is.na(resultado)) {
+      numero_chip <- resultado[2]
+      chip2 <- c(chip2, numero_chip)
     }
   }
 }
-console.log(chip2);
+print(chip2)
 
-const ficheros = LeerFicherosPDF(rutaEntrada);
-let max_mut = 0;
-const genes_mut2 = {};
-const frecuencias_totales = [];
-const patron_frecuencia = /\d{2}\.\d{2}/;
-for (const ficheroPDF of ficheros) {
-  const nombreFichero = os.path.normpath(os.path.join(rutaEntrada, ficheroPDF));
-  const lines = LeerDocumento(nombreFichero);
-  let total_mut = 0;
-  const encontrados2 = [];
-  const lista_frec = [];
-  for (const mutacion of mutaciones) {
-    if (lines.includes(mutacion)) {
-      const posicion = lines.indexOf(mutacion);
-      if (mutacion === "FGFR4") {
-        if (posicion < lines.length - 1 && lines[posicion + 1] === "p.(P136L)") {
-          continue;
+ficheros <- LeerFicherosPDF(rutaEntrada)
+max_mut <- 0
+genes_mut2 <- list()
+frecuencias_totales <- list()
+patron_frecuencia <- "\\d{2}\\.\\d{2}"
+for (ficheroPDF in ficheros) {
+  nombreFichero <- file.path(rutaEntrada, ficheroPDF)
+  lines <- LeerDocumento(nombreFichero)
+  total_mut <- 0
+  encontrados2 <- character()
+  lista_frec <- character()
+  for (mutacion in mutaciones) {
+    if (mutacion %in% lines) {
+      posicion <- which(lines == mutacion)
+      if (mutacion == "FGFR4") {
+        if (posicion < length(lines) && lines[posicion + 1] == "p.(P136L)") {
+          next
         }
-      } else {
-        let benigno = false;
-        for (let a = posicion + 1; a < posicion + 10; a++) {
-          if (lines[a].includes("Benign")) {
-            benigno = true;
-          }
+      }
+      total_mut <- total_mut + 1
+      encontrados2 <- c(encontrados2, mutacion)
+    } else {
+      benigno <- FALSE
+      for (a in (posicion + 1):(posicion + 10)) {
+        if ("Benign" %in% lines[a]) {
+          benigno <- TRUE
         }
-        if (!benigno) {
-          total_mut += 1;
-        }
-        encontrados2.push(mutacion);
-        console.log(nombreFichero + " - Existe: " + mutacion);
-        if (!benigno) {
-          for (const i of lines.slice(posicion, posicion + 10)) {
-            const resultado = i.match(patron_frecuencia);
-            if (resultado) {
-              const frec = resultado[0];
-              lista_frec.push(frec);
-            }
-          }
+      }
+      if (!benigno) {
+        total_mut <- total_mut + 1
+        encontrados2 <- c(encontrados2, mutacion)
+      }
+    }
+    print(paste(nombreFichero, "- Existe:", mutacion))
+    if (!benigno) {
+      for (i in lines[(posicion):(posicion + 10)]) {
+        resultado <- str_match(i, patron_frecuencia)
+        if (!is.na(resultado)) {
+          frec <- resultado[1]
+          lista_frec <- c(lista_frec, frec)
         }
       }
     }
   }
-  genes_mut2[ficheroPDF.replace("\\", "_")] = encontrados2;
+  genes_mut2[[gsub("\\\\", "_", ficheroPDF)]] <- encontrados2
   if (total_mut > max_mut) {
-    max_mut = total_mut;
+    max_mut <- total_mut
   }
-  frecuencias_totales.push(lista_frec);
+  frecuencias_totales <- c(frecuencias_totales, lista_frec)
 }
-console.log(frecuencias_totales);
-const mut = Object.values(genes_mut2);
-console.log(mut);
-const num_mutaciones = mut.map(i => i.length);
-console.log(num_mutaciones);
-const numero_iden = mut.map(i => i.map(gen => mutaciones_dic[gen] || 0));
-console.log(numero_iden);
+print(frecuencias_totales)
 
-const fusiones = [];
-for (const ficheroPDF of ficheros) {
-  const lines = LeerDocumento(os.path.normpath(os.path.join(rutaEntrada, ficheroPDF)));
-  const variantes = [];
-  for (const linea of lines) {
-    for (const mutacion of mutaciones) {
-      const patronGen = new RegExp("[A-Z0-9]{1,}-" + mutacion);
-      // continue with the rest of the code
+mut <- unlist(genes_mut2)
+print(mut)
+
+num_mutaciones <- sapply(mut, length)
+print(num_mutaciones)
+
+numero_iden <- lapply(mut, function(x) {
+  sapply(x, function(gen) {
+    mutaciones_dic[[gen]]
+  })
+})
+print(numero_iden)
+
+fusiones <- character()
+for (ficheroPDF in ficheros) {
+  lines <- LeerDocumento(file.path(rutaEntrada, ficheroPDF))
+  variantes <- character()
+  for (linea in lines) {
+    for (mutacion in mutaciones) {
+      patronGen <- paste0("[A-Z0-9]{1,}-", mutacion)
+      if (grepl(patronGen, linea)) {
+        variantes <- c(variantes, linea)
+      }
     }
   }
+  fusiones <- c(fusiones, variantes)
 }
 
 
